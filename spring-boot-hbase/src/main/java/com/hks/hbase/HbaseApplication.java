3c41a5199f49ede82be4fffb999216a44eb62db1
heks <heks@yiche.com>
2018-11-29 11:04:48 +0800

    1.spring-boot-hbase

---
+++ spring-boot-hbase/src/main/java/com/hks/hbase/HbaseApplication.java
@@ -0,0 +1,12 @@
+package com.hks.hbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbaseApplication.class, args);
+	}
+}
