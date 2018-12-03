ceff6f70eb99d1a2734c76c2bd599c98a5a347da
heks <heks@yiche.com>
2018-12-03 11:27:18 +0800

    init hive

---
+++ spring-boot-hive/src/main/java/com/hks/hive/SpringBootHiveApplication.java
@@ -0,0 +1,12 @@
+package com.hks.hive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHiveApplication.class, args);
+	}
+}
