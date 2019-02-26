3a7c4dd35305d015943339bfcc0bd57d1d207891
heks <heks@yiche.com>
2019-02-26 10:04:38 +0800

    添加springboot+spark的项目脚手架

---
+++ spring-boot-spark/src/main/java/com/hks/spark/SparkApplication.java
@@ -0,0 +1,13 @@
+package com.hks.spark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkApplication.class, args);
+	}
+
+}
---
+++ spring-boot-spark/src/test/java/com/hks/spark/SparkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hks.spark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SparkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
