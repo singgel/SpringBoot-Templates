7643ccd34ac291ef16fb897a733994f3c830415d
hekuangsheng <hekuangsheng@xueqiu.com>
2019-05-14 20:27:19 +0800

    spring-boot-prometheus-exporter

---
+++ spring-boot-prometheus-exporter/src/main/java/com/hks/exporter/ExporterApplication.java
@@ -0,0 +1,13 @@
+package com.hks.exporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExporterApplication.class, args);
+	}
+
+}
